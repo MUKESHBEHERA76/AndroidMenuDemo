3bddcf052a2f68ab23ca900e50fabfcc471d92ab
Mukesh Kumar Behera <61012269+MUKESHBEHERA76@users.noreply.github.com>
2025-01-10 20:54:08 +0530

    Initial commit

---
+++ app/src/main/java/com/harmonigate/androidmenudemo/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.harmonigate.androidmenudemo.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
